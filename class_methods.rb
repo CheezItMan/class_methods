a6fcfe831404adf59a7614443dae3726500a45e0
Chris <chris@adadevelopersacademy.org>
2017-02-22 10:44:57 -0800

    Finished part 1

--- class_methods.rb
+++ class_methods.rb
@@ -1,3 +1,7 @@
+require 'awesome_print'
+
+
+# Planet Example - Review initialize
 class Planet
   attr_reader :name, :moons
 
@@ -8,5 +12,65 @@ class Planet
 
   def get_info
     "I am a planet whose name is #{ @name } and has #{ @moons }."
+  end
+end
+
+
+mars = Planet.new("mars", 1)
+puts mars.get_info
+
+
+## Order Example - go over snake_case_names
+
+# class Order
+#   def initialize(subtotal, quantity) # <= instance method
+#     @subtotal = subtotal
+#     @quantity = quantity
+#   end
+#
+#   def total # <= instance method
+#     @subtotal * @quantity
+#   end
+#
+#   def to_money # <= instance method
+#     # in the line below, `total` is invoking the instance method above
+#     "$" + sprintf("%0.02f", total / 100)
+#   end
+# end
+#
+#
+# my_order = Order.new(300, 2)
+# my_order.to_money
+
+
+# Coordinate example - Review Attributes
+#  Getter & Setter Methods
+
+# Using attr_accessor
+# class Coordinate
+#   attr_accessor :x, :y
+#
+#   def initialize(x, y)
+#     @x = x
+#     @y = y
+#   end
+# end
+#
+# point1 = Coordinate.new(100, 50)
+# point2 = Coordinate(75, 30)
+# point1.x += 10
+# puts point2.y
+#
+## Above we have to create an instance of our class before calling a method.
+
+
+## 1st Example Class method
+class TryingThisOut
+  def self.cool
+    "This is a CLASS METHOD"
+  end
+
+  def rad
+    "This is an INSTANCE METHOD"
   end
 end
